90779db5ec4978634d5f6c999d7603279c809837
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-02-19 14:25:39 +0900

    add create room feature

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
+from app.api.meeting_room import router
 from app.core.config import settings
 
 
 app = FastAPI(title=settings.app_title)
+
+app.include_router(router)
