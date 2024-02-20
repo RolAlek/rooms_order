ef9af29bd11de8334beae85f52309b6be657303c
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-02-20 11:55:05 +0900

    create base crud, ref room crud and api

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -2,10 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
-from app.crud.meeting_room import (
-    create_meeting_room, delete_meeting_room, get_room_id_by_name,
-    get_meeting_room_by_id, read_all_rooms_from_db, update_meeting_room
-)
+from app.crud.meeting_room import meeting_room_crud
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import (
     MeetingRoomCreate, MeetingRoomDB, MeetigRoomUpdate
@@ -16,7 +13,7 @@ router = APIRouter(prefix='/meeting_rooms', tags=['Meeting Rooms'])
 
 
 async def check_name_duplicate(room_name: str, session: AsyncSession) -> None:
-    room_id = await get_room_id_by_name(room_name, session)
+    room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
     if room_id:
         raise HTTPException(
             status_code=422,
@@ -28,7 +25,7 @@ async def check_meeting_room_exists(
     meeting_room_id: int,
     session: AsyncSession
 ) -> MeetingRoom:
-    meeting_room = await get_meeting_room_by_id(meeting_room_id, session)
+    meeting_room = await meeting_room_crud.get(meeting_room_id, session)
 
     if not meeting_room:
         raise HTTPException(
@@ -48,7 +45,7 @@ async def create_new_meeting_room(
     session: AsyncSession = Depends(get_async_session)
 ):
     await check_name_duplicate(meeting_room.name, session)
-    new_room = await create_meeting_room(meeting_room, session)
+    new_room = await meeting_room_crud.create(meeting_room, session)
     return new_room
 
 
@@ -60,14 +57,14 @@ async def create_new_meeting_room(
 async def get_all_meeting_rooms(
     session: AsyncSession = Depends(get_async_session)
 ):
-    all_rooms = await read_all_rooms_from_db(session)
+    all_rooms = await meeting_room_crud.get_multi(session)
     return all_rooms
 
 
 @router.patch(
     path='/{meeting_room_id}',
     response_model=MeetingRoomDB,
-    response_model_exclude_none=True    
+    response_model_exclude_none=True
 )
 async def partially_update_meeting_room(
     meeting_room_id: int,
@@ -79,7 +76,9 @@ async def partially_update_meeting_room(
     if obj_in.name:
         await check_name_duplicate(obj_in.name, session)
 
-    meeting_room = await update_meeting_room(meeting_room, obj_in, session)
+    meeting_room = await meeting_room_crud.update(
+        meeting_room, obj_in, session
+    )
     return meeting_room
 
 
@@ -93,5 +92,5 @@ async def remove_meeting_room(
     session: AsyncSession = Depends(get_async_session)
 ):
     meeting_room = await check_meeting_room_exists(meeting_room_id, session)
-    meeting_room = await delete_meeting_room(meeting_room, session)
+    meeting_room = await meeting_room_crud.remove(meeting_room, session)
     return meeting_room
